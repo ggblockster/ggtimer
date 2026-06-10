e30a9499dfb3d350247f89555c35c9e4ac519d53
ggblockster <ggblockster@gmail.com>
2026-06-10 18:28:34 +1000

    Update shortcuts.js

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -10,6 +10,16 @@ openShortcuts.addEventListener("click", () => {
     sOverMenu.style.display = 'grid';
 });
 
+document.addEventListener("keydown", function(e) {
+    if (e.code === 'KeyI') {
+        if (sOverMenu.style.display == 'none') {
+            sOverMenu.style.display = 'grid';
+        } else {
+            sOverMenu.style.display = 'none';
+        }
+    }
+});
+
 sOverMenu.addEventListener("click", (evt) => {
   if (!evt.target.closest("#sMenu")) sOverMenu.style.display = 'none';
 });
