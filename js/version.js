0a252203019b31af7770be3e7005b6f97dadd7ca
ggblockster <ggblockster@gmail.com>
2026-06-10 13:36:53 +1000

    added beta flag

--- js/version.js
+++ js/version.js
@@ -2,8 +2,12 @@ const version = 'v1.2.6';
 const vtext = document.getElementById("version");
 
 vtext.textContent = version;
-document.title = 'ggTimer ' + version;
+if (window.location.hostname === '127.0.0.1') {
+   document.title = 'ggTimer ' + version + '-beta';
+} else {
+   document.title = 'ggTimer ' + version;
+}
 
 const titleObj = document.getElementById("title");
 titleObj.title = `< - - - - - ggTimer ${version} - - - - - - >
-   A simple, general-purpose timer`
+   A simple, general-purpose timer`;
